21c15f05fd1e223cbed95bb32d74bbe2d6ad0d1d
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2025-12-01 02:32:09 +0100

    1. Added "Book list page" 2. Added "Create add book page" 3. Added "Create edit book page" 4. Added "Delete book function"

--- client/src/app/pages/books/book-add/book-add.component.ts
+++ client/src/app/pages/books/book-add/book-add.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BookService } from '../../../services/book.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-book-add',
   standalone: true,
-  template: `
-    <h2>Add New Book</h2>
-    <!-- Form will go here -->
-  `
+  imports: [ReactiveFormsModule, CommonModule, RouterLink, FontAwesomeModule],
+  templateUrl: './book-add.component.html'
 })
-export class BookAddComponent {}
+export class BookAddComponent {
+
+  // Icon for "Add"
+  faPlus = faPlus;
+
+  // Create form with validation rules
+  form: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private bookService: BookService,
+    private router: Router
+  ) {
+    // Build the form fields
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      publishedDate: ['', Validators.required]
+    });
+  }
+
+  // Sends POST to create a new book.
+  onSubmit(): void {
+    if (this.form.invalid) return; // Stop when form not valid
+
+    // Build request object
+    const data = {
+      title: this.form.value.title ?? '',
+      author: this.form.value.author ?? '',
+      publishedDate: this.form.value.publishedDate ?? ''
+    };
+
+    // Send POST request to API
+    this.bookService.createBook(data).subscribe({
+      next: () => {
+        alert('Bok skapad!');  // Tell the user that the book is created
+        this.router.navigate(['/books']); // Go back to list
+      },
+      error: () => {
+        alert('Kunde inte skapa boken.');
+      }
+    });
+  }
+}
+
+
